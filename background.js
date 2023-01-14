a417d23781b5631d7418603342d02e67973fe57c
harshpatel <harsh.patel991@gmail.com>
2023-01-14 13:42:00 -0600

    Update for new teleparty, update to manifest v3

--- background.js
+++ background.js
@@ -4,11 +4,13 @@ function myMain(evt) {
     var jsInitChecktimer = setInterval(checkForJS_Finish, 1000);
 
     function checkForJS_Finish() {
-        var chatHistoryContainer = window.document.getElementById('chat-history-container');
+        var chatHistoryContainer = document.querySelectorAll('div[class^="onboard_sidebar-history-container"]')[0];
 
         if (chatHistoryContainer != null) {
             console.log("Teleparty detected");
             clearInterval(jsInitChecktimer);
+            console.log(chatHistoryContainer);
+            console.log(Notification.permission);
 
 
             if (Notification.permission !== 'denied' || Notification.permission === "default") {
@@ -17,23 +19,66 @@ function myMain(evt) {
                 });
             }
 
-            elementToObserve = chatHistoryContainer.children[0];
+            var elementToObserve = chatHistoryContainer.children[0];
+            console.log("ement to observe");
+            console.log(elementToObserve);
             observer = new MutationObserver(function (mutationsList, observer) {
-                var nickNamePlaceholder = window.document.getElementById('nickname-edit').placeholder;
-                var nickNameValue = window.document.getElementById('nickname-edit').value;
-                var nickName = nickNameValue === '' ? nickNamePlaceholder : nickNameValue;
+                var profileIcon = window.document.querySelector("[tp-id=edit_profile]").src;
 
                 var messageParts = mutationsList[0].addedNodes[0].innerText.split("\n");
-                var name = messageParts[0];
                 var message = messageParts[2];
-                if (name != nickName) {
+
+                var foundProfileNameAndIcon = findProfileNameAndIcon(mutationsList[0].addedNodes[0]);
+
+                var name = foundProfileNameAndIcon.name;
+                var messageProfileIcon = foundProfileNameAndIcon.messageProfileIcon;
+
+                if(messageProfileIcon !== profileIcon) {
                     var audio = new Audio('https://actions.google.com/sounds/v1/cartoon/wood_plank_flicks.ogg');
                     audio.play();
-                    new Notification(name, { body: message });
+                    new Notification(name, {body: message});
                 }
             });
 
             observer.observe(elementToObserve, { characterData: false, childList: true, attributes: false });
+        } else {
+            console.log("Teleparty not detected");
         }
     }
+}
+
+//move up the siblings
+function findProfileNameAndIcon(element) {
+    //loop over all classes for the element, if it has "combined", we need to look at earlier siblings which don't have "combined"
+    if(isCombinedElement(element)) {
+        let previousSiblings = getPreviousSiblings(element);
+        for(const previousSibling of previousSiblings) {
+            if(!isCombinedElement(previousSibling)) {
+                return {name: previousSibling.innerText.split("\n")[0], messageProfileIcon: previousSibling.childNodes[0].src } ;
+            }
+        }
+    } else {
+        return {name: element.innerText.split("\n")[0], messageProfileIcon: element.childNodes[0].src } ;
+    }
+}
+
+function isCombinedElement(element) {
+    var classes = element.className.split(' ');
+
+    for (var i = 0; i < classes.length; i++) {
+        var name = classes[i];
+        if (name.includes('combined')) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function getPreviousSiblings(elem, filter) {
+    var sibs = [];
+    while (elem = elem.previousSibling) {
+        if (elem.nodeType === 3) continue; // text node
+        if (!filter || filter(elem)) sibs.push(elem);
+    }
+    return sibs;
 }
